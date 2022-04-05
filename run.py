11a3e8e433068e3842c6cfa945f50aeb43470fe7
alternistom <tocy777@gmail.com>
2022-04-05 09:38:18 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,20 +4,30 @@ import os.path, sys
 path = "rplace_arch_images_here"
 cropped = "rplace_arch_images_here\\cropped"
 resized = "rplace_arch_images_here\\cropped\\resized"
+
+if not os.path.exists(path):
+				os.makedirs(path)
+        
+if not os.path.exists(cropped):
+				os.makedirs(cropped)
+        
+if not os.path.exists(resized):
+				os.makedirs(resized)
+        
 dirs = os.listdir(path)
 
 counter = 0
 
 def cropAndResize(counter: int):
     for item in dirs:
-        fullpath = os.path.join(path,item)         #corrected
+        fullpath = os.path.join(path,item)
         if os.path.isfile(fullpath):
 
             counter += 1
 
             im = Image.open(fullpath)
             f, e = os.path.splitext(fullpath)
-            imCrop = im.crop((535, 70, 894, 252)) #corrected
+            imCrop = im.crop((535, 70, 894, 252))
             imCrop.save(cropped + "\\cropped_" + item, "PNG", quality=100)
             
             basewidth = 3840
